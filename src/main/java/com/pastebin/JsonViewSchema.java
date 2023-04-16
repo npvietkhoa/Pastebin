6ddb26c9b82b86b813de643749c3fdf0e80059d9
npvietkhoa <npvietkhoa@gmail.com>
2023-04-16 02:02:22 +0200

    get rid of view schema - sometimes cause problems while viewing

--- src/main/java/com/pastebin/JsonViewSchema.java
+++
@@ -1,9 +0,0 @@
-package com.pastebin;
-
-
-public class JsonViewSchema {
-    public static class postView{}
-
-    public static class getView{}
-    public static class fullView extends postView{}
-}
