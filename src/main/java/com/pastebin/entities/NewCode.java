36c207435ad452dfebd16b4245fdbf7f7d93d6f6
npvietkhoa <npvietkhoa@gmail.com>
2023-04-16 02:02:58 +0200

    class to get new code from json with object mapper

---
+++ src/main/java/com/pastebin/entities/NewCode.java
@@ -0,0 +1,12 @@
+package com.pastebin.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewCode {
+    private String code;
+}
