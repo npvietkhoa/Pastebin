5659d5bc4d51a7d90a09c8b0ccb48d3dcdaa5abd
npvietkhoa <npvietkhoa@gmail.com>
2023-04-16 02:01:09 +0200

    change snippet to entity

--- src/main/java/com/pastebin/entities/Snippet.java
+++ src/main/java/com/pastebin/entities/Snippet.java
@@ -1,54 +1,31 @@
 package com.pastebin.entities;
 
-import com.fasterxml.jackson.annotation.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import com.pastebin.JsonViewSchema;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.ZoneId;
 
 @Data
-@AllArgsConstructor
+@Entity
 @NoArgsConstructor
 public class Snippet {
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private Long id;
 
-    @JsonIgnore
-    private int id;
-
-
-
-    @JsonPropertyOrder({
-            "id",
-            "code",
-            "date"
-    })
-
-    @JsonView(JsonViewSchema.postView.class)
-    @JsonProperty("id")
-    public String stringifyId() {
-        return Integer.toString(this.id);
-    }
-
-    @JsonView({JsonViewSchema.getView.class, JsonViewSchema.fullView.class})
-    @JsonProperty("code")
     private String code;
 
-    private LocalDateTime createdDateTime;
-
-    @JsonView({JsonViewSchema.getView.class, JsonViewSchema.fullView.class})
-    @JsonFormat(pattern = "yyyy/MM/dd HH:mm:ss")
-    @JsonProperty("date")
-    public String formatCreatedDateTime() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        return this.createdDateTime.format(formatter);
-    }
-
+    @Setter(AccessLevel.NONE)
+    private Timestamp createdDateTime = Timestamp.valueOf(LocalDateTime.now());
     public Snippet(String code) {
         this.code = code;
-        this.createdDateTime = LocalDateTime.now();
     }
+
 }
 
 
