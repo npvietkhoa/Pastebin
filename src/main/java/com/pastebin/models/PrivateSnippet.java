14bb4f2482e220a22afe699ef785b8e0d342e342
npvietkhoa <npvietkhoa@gmail.com>
2023-05-18 22:59:10 +0200

    comment out/delete some stuff

---
+++ src/main/java/com/pastebin/models/PrivateSnippet.java
@@ -0,0 +1,16 @@
+//package com.pastebin.models;
+//
+//import jakarta.persistence.Entity;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.EqualsAndHashCode;
+//import lombok.NoArgsConstructor;
+//
+//@EqualsAndHashCode(callSuper = true)
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Data
+//@Entity
+//public class PrivateSnippet extends LimitedSnippet{
+//    private String snippetHash;
+//}
--- src/test/java/com/pastebin/PastebinApplicationTests.java
+++ src/test/java/com/pastebin/PastebinApplicationTests.java
@@ -1,13 +1,32 @@
-package com.pastebin;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PastebinApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.pastebin;
+//
+//import com.pastebin.models.LimitedSnippet;
+//import com.pastebin.models.PublicSnippet;
+//import com.pastebin.repositories.PublicSnippetRepository;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import static org.junit.jupiter.api.Assertions.assertSame;
+//
+//@SpringBootTest
+//class PastebinApplicationTests {
+//
+//    final private PublicSnippetRepository publicSnippetRepository;
+//
+//    PastebinApplicationTests(PublicSnippetRepository publicSnippetRepository) {
+//        this.publicSnippetRepository = publicSnippetRepository;
+//    }
+//
+//
+//    @Test
+//    void addCode() throws Exception {
+////        UUID randomUUID = UUID.randomUUID();
+////        UUID id = snippetRestController.addNewSnippet(String.valueOf(randomUUID));
+////        assertSame(String.valueOf(randomUUID), snippetRestController.getSnippetJsonById(String.valueOf(id)).getCode());
+////        Snippet jdbc = new Snippet("asdf");
+//        LimitedSnippet private_snippet = new LimitedSnippet(1, 1);
+//        PublicSnippet public_snippet = new PublicSnippet("asdf");
+//
+////        publicSnippetRepository.save(private_snippet);
+//    }
+//}
