f1e0974a60db1c0ecc793ad52ad3713cc3684aa5
npvietkhoa <npvietkhoa@gmail.com>
2023-05-24 14:48:12 +0200

    optimize import

--- src/main/java/com/pastebin/controllers/api/SnippetRestController.java
+++ src/main/java/com/pastebin/controllers/api/SnippetRestController.java
@@ -1,18 +1,13 @@
 package com.pastebin.controllers.api;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pastebin.models.SnippetDTO;
-import com.pastebin.models.SnippetDTOMapper;
-import com.pastebin.services.LimitedSnippetService;
-import com.pastebin.services.PublicSnippetService;
 import com.pastebin.services.SnippetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/api/code/", produces = MediaType.APPLICATION_JSON_VALUE)
